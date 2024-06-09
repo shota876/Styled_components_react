76962053d73ab0af142896e1b0aa4eb56b14399c
shota876 <sh.guliashvili1@gmail.com>
2024-06-09 05:22:07 -0700

    navbar added.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,76 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    width: 100%;
-    height: 10%;
+  width: 100%;
+  height: 10%;
+  background-color: aliceblue;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Logo = styled.h1`
+  font-size: 20px;
+  text-decoration: underline;
+  color: darkblue;
+  cursor: pointer;
+`;
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+
+const MenuItem = styled.li`
+  font-size: 20px;
+  font-weight: bold;
+  margin-right: 20px;
+  color: gray;
+  cursor: pointer;
+  &:hover {
+    color: darkblue;
+  }
+`;
+
+const Button = styled.button`
+  font-weight: bold;
+  background-color: darkblue;
+  border: 2px solid white;
+  cursor: pointer;
+  color: white;
+  padding: 10px 10px;
+  border-radius: 10px;
+  &:hover{
     background-color: aliceblue;
+    color: darkblue;
+    border: 2px solid darkblue;
+  }
 `
 
-const Left = styled.div`
-    
-`
-const Center = styled.div`
-    
-`
-const Right = styled.div`
-    
-`
+const Left = styled.div``;
+const Center = styled.div``;
+const Right = styled.div``;
 
 function Navbar() {
-  return(
+  return (
     <>
-       <Container>
-          <Left>Left</Left>
-          <Center>Center</Center>
-          <Right>Right</Right>
-       </Container>
-    </> 
-  )
-    
+      <Container>
+        <Left>
+          <Logo>IT Support</Logo>
+        </Left>
+        <Center>
+          <Menu>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>About Us</MenuItem>
+            <MenuItem>Services</MenuItem>
+            <MenuItem>Contact</MenuItem>
+            <MenuItem>Blog</MenuItem>
+          </Menu>
+        </Center>
+        <Right><Button>call us now</Button></Right>
+      </Container>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
